5a0f7f86f523b872e6a62b6db6c5beb40735791c
Cory Lynch <cjl2625@gmail.com>
2015-02-01 01:16:10 -0500

    Uploaded old python client, will merge with scout_ui.py

---
+++ old_client.py
@@ -0,0 +1,31 @@
+import bluetooth
+
+server_address = None
+
+print('searching for server')
+nearby_devices = bluetooth.discousuinver_devices()
+
+for address in nearby_devices:
+    if bluetooth.lookup_name(address, 8) == 'APPLEPI-PC':
+        server_address = address
+        break
+
+if server_address is not None:
+    print('found server with address', server_address)
+else:
+    print('could not find server')
+    exit()
+
+socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+socket.connect((server_address, 27))
+print('successfully connected to server')
+
+def send(msg):
+    socket.send(msg)
+    print('sent: ', msg)
+
+send('test 1')
+send('test 2')
+send('close')
+
+socket.close()
